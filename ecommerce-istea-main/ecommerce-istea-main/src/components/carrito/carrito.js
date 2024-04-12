37c25c7201800b7264e6c8dcd0e8d752e34bf5df
lucasdoro <146653626+lucasdoro@users.noreply.github.com>
2024-04-12 10:13:35 -0300

    Add files via upload

---
+++ ecommerce-istea-main/ecommerce-istea-main/src/components/carrito/carrito.js
@@ -0,0 +1,18 @@
+
+
+export function carrito(){
+    const array = [];
+
+    const btn = document.querySelectorAll(`#btn-${item.id}`);
+    btn.forEach((element)=>{
+        element.addEventListener('click',()=>{
+            const productoSeleccionado = element => element.id === btn.id;
+            
+            
+            
+        })
+    })
+
+
+
+}
